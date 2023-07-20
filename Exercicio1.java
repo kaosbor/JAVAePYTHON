1b9f96a3cfe5b401b39367610baf0de2d7a4e6ed
Rodrigo Borges <119867556+kaosbor@users.noreply.github.com>
2023-07-20 11:56:38 -0300

    20/07/2023 11:56

--- Exercicio1.java
+++ Exercicio1.java
@@ -1,90 +1,129 @@
-import java.io.PrintStream;
-import java.util.Scanner;
+import java.io.PrintStream; // Para aparecer na tela do print
+import java.util.Scanner; // Para receber dados
+import java.util.Random; // Para gerar numerais aleatórios
 
 public class Exercicio1 {
-    public static void main(String[] Declarar) {
-        PrintStream out = System.out;
-        out.println("\033[h\033[2J");
 
+    public static int gerarNumeroAleatorio(int min, int max) { // Outro método - nome gerarNumeroAleatorio
+        Random random = new Random();
+        return random.nextInt((max - min) + 1) + min;
+    }
+
+    public static void main(String[] DECLARAR) { // método - main é palavra reservada
+        PrintStream out = System.out;
+        out.println("\033[h\033[2J"); // LIMPA A TELA
         Scanner scanner = new Scanner(System.in);
 
         // Exercício 1 (Nível Fácil - Variáveis):
-            out.println("Exercício 1 (Nível Fácil - Variáveis):");
+        out.println("Exercício 1 (Nível Fácil - Variáveis):");
+        out.println("Digite um nota número: ");
 
-            out.print("Digite um nota número: ");
-            double numero = scanner.nextDouble();
-            out.println("O número declarado foi " + numero + ".");
+        int numero0 = gerarNumeroAleatorio(1, 100);
+        out.println("O número declarado foi " + numero0 + ".");
 
         // Exercício 2 (Nível Fácil - Variáveis):
-            out.print("\n");
-            out.println("Exercício 2 (Nível Fácil - Variáveis):");
+        out.print("\n");
+        out.println("Exercício 2 (Nível Fácil - Variáveis):");
 
-            out.print("Digite o 1º número: ");
-            double numero1 = scanner.nextDouble();
-            out.println("O 1º número declarado foi o nº " + numero1 + ".");
-            
-            out.print("Digite o 2º número: ");
-            double numero2 = scanner.nextDouble();
-            out.println("O 2º número declarado foi o nº" + numero2 + ".");
-            
-            double soma = numero1 + numero2;
-            
-            out.println("A soma do número " + numero1 + " + " + numero2 + " foi convertida para " + soma + ".");
+        out.print("Digite o 1º número: ");
+        // int numero1 = scanner.nextDouble();
+        int numero1 = gerarNumeroAleatorio(1, 100);
+        out.println("O número declarado foi " + numero1 + ".");
+
+        out.print("Digite o 2º número: ");
+        // int numero2 = scanner.nextDouble();
+        int numero2 = gerarNumeroAleatorio(1, 100); // Método + Dados fora dele = Objeto
+        out.println("O número declarado foi " + numero2 + ".");
+
+        out.println("O 1º número declarado foi o nº " + numero1 + ".");
+        out.println("O 2º número declarado foi o nº " + numero2 + ".");
+
+        double soma = numero1 + numero2;
+
+        out.println("A soma dos números " + numero1 + " + " + numero2 + " é " + soma + ".");
 
         // Exercício 3 (Nível Fácil - Loops):
-            out.print("\n");
-            out.println("Exercício 3 (Nível Fácil - Loops):");
+        out.print("\n");
+        out.println("Exercício 3 (Nível Fácil - Loops):");
 
-            out.print("Digite um número para início: ");
-            int inicio = scanner.nextInt();
+        out.print("Digite um número para início: ");
+        int inicio = scanner.nextInt();
 
-            out.print("Digite um número para limite: ");
-            int limite = scanner.nextInt();
+        out.print("Digite um número para limite: ");
+        int limite = scanner.nextInt();
 
-            for (int i = inicio; i <= limite; i++) {
-                out.println(i);
-            }
-
+        for (int i = inicio; i <= limite; i++) {
+            out.print(i + " ");
+        }
+        out.print("\n");
         out.println("O número iniciou em " + inicio + " e terminou em " + limite + ".");
 
         // Exercício 4 (Nível Fácil - Loops - Soma):
-            out.print("\n");
-            out.println("Exercício 4 (Nível Fácil - Loops - Soma):");
-            out.print("Digite um número para início: ");
-            int iniciar = scanner.nextInt();
+        out.print("\n");
+        out.println("Exercício 4 (Nível Fácil - Loops - Soma):");
 
-            out.print("Digite um número para limite: ");
-            int limitar = scanner.nextInt();
+        out.print("Digite um número para início: ");
+        int iniciar = scanner.nextInt();
 
-            for (int i = iniciar; i <= limitar; i++) {
-                /* out.println(i); { */
-                    soma = soma + i; // ou += soma foi convertida para soma + i 
-                }
+        out.print("Digite um número para limite: ");
+        int limitar = scanner.nextInt();
 
-            out.println("A soma desde o inicio em " + iniciar + " com término em " + limitar + " foi convertida para " + soma + ".");
+        int soma1 = 0;
+
+        for (int i = 1; i <= limitar; i++) {
+            out.println("A soma se refere aos seguintes números " + i);
+            soma1 = soma1 + i; // ou += soma foi convertida para soma + i
+        }
+
+        out.println("O número iniciou em " + iniciar + " e terminou em " + limitar + ".");
+        out.println("A soma dos números do inicio em " + iniciar + " com término em " + limitar
+                + " foi convertida para " + soma1 + ".");
 
         // Exercício 5 (Nível Fácil - Condicionais):
-            out.print("\n");
-            out.println("Exercício 5 (Nível Fácil - Condicionais):");
+        out.print("\n");
+        out.println("Exercício 5 (Nível Fácil - Condicionais):");
 
-            out.print("Digite a nota do aluno: ");
-            double nota = scanner.nextDouble();
+        out.print("Digite a nota do aluno: ");
+        double nota = scanner.nextDouble();
 
-            if (nota >= 9) {
-                out.println("A nota do aluno " + nota + " foi convertida para 'A'.");
-            } else if (nota >= 8) {
-                out.println("A nota do aluno " + nota + " foi convertida para 'B'.");
-            } else if (nota >= 7) {
-                out.println("A nota do aluno " + nota + " foi convertida para 'C'.");
-            } else if (nota >= 6) {
-                out.println("A nota do aluno " + nota + " foi convertida para 'D'.");
-            } else if (nota >=5) {
-                out.println("A nota do aluno " + nota + " foi convertida para 'E'");
-            } else {
-                out.println("Não ira ganhar TORTUGUITA!");
-            }
+        if (nota >= 9) {
+            out.println("A nota do aluno " + nota + " foi convertida para 'A'.");
+        } else if (nota >= 8) {
+            out.println("A nota do aluno " + nota + " foi convertida para 'B'.");
+        } else if (nota >= 7) {
+            out.println("A nota do aluno " + nota + " foi convertida para 'C'.");
+        } else if (nota >= 6) {
+            out.println("A nota do aluno " + nota + " foi convertida para 'D'.");
+        } else if (nota >= 5) {
+            out.println("A nota do aluno " + nota + " foi convertida para 'E'");
+        } else {
+            out.println("Não irá ganhar TORTUGUITA!");
+        }
 
+        out.print("\n");
+
+        out.println("Exercício 6 (Nível Fácil - Variáveis - Fatorial:");
+        out.print("Digite um número fatorial: ");
+        int Receber = scanner.nextInt();
+        CalcularFatorial(Receber);
 
         scanner.close();
+    }
+
+    public static void CalcularFatorial(int Numero) {
+        // Exercício 6 (Nível Médio - Variáveis - Fatorial):
+        PrintStream out = System.out;
+        int fatorial = 1;
+
+        for (int i = 1; i <= Numero; i++) {
+            out.println(i);
+            fatorial = fatorial * i; // ou += soma foi convertida para soma + i
+        }
+
+        // printf = printformat chama a variável %d que já é da linguagem (inteiro)
+        // variável (fatorial) e o parâmetro (Numero)
+        out.printf("O fatorial de %d é " + fatorial, Numero);
+        out.print("\n");
+        out.printf("O fatorial do numero " + Numero + " é " + fatorial);
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # JAVAePYTHON
  JAVAePYTHON
 
- 
+Exercícios de Java e Python:
+
+Exercício 1 (Nível Fácil - Variáveis):
+Escreva um programa em Java que declare uma variável inteira e a imprima na tela.
+Dica: Declare uma variável do tipo inteiro utilizando o formato `tipo nomeDaVariavel = valor;` e utilize o comando `System.out.println()` para exibir o valor da variável.
+
+Exercício 2 (Nível Fácil - Variáveis):
+Crie um programa em Java que declare duas variáveis do tipo inteiro, realize a soma entre elas e exiba o resultado.
+Dica: Declare duas variáveis do tipo inteiro utilizando o formato `tipo nomeDaVariavel = valor;`, realize a soma entre elas utilizando o operador `+` e exiba o resultado utilizando o comando `System.out.println()`.
+
+Exercício 3 (Nível Fácil - Loops):
+Implemente um programa em Java que exiba na tela os números de 1 a 10.
+Dica: Utilize um loop `for` ou `while` para percorrer os números de 1 a 10. Declare uma variável de controle no loop e utilize o comando `System.out.println()` dentro do loop para exibir cada número.
+
+Exercício 4 (Nível Fácil - Loops):
+Crie um programa em Java que calcule a soma dos números de 1 a 100 e exiba o resultado.
+Dica: Utilize um loop `for` para percorrer os números de 1 a 100. Declare uma variável acumuladora antes do loop e utilize o operador `+=` para ir acumulando a soma dos números dentro do loop. No final, exiba o valor da variável acumuladora utilizando o comando `System.out.println()`.
+
+Exercício 5 (Nível Fácil - Condicionais):
+Escreva um programa em Java que verifique se um número é par ou ímpar e exiba o resultado.
+Dica: Utilize uma estrutura condicional `if` para verificar se o número é par ou ímpar. Utilize o operador `%` para verificar se o número é divisível por 2. Se o resto for igual a 0, é par; caso contrário, é ímpar. Utilize o comando `System.out.println()` para exibir o resultado.
+
+Exercício 6 (Nível Médio - Variáveis):
+Implemente um programa em Java que calcule e exiba o valor do fatorial de um número digitado pelo usuário.
+Dica: Utilize um loop `for` para calcular o fatorial de um número. Inicialize uma variável fatorial com o valor 1 antes do loop e, dentro do loop, multiplique o valor atual do fatorial pelo número em cada iteração. No final, exiba o valor do fatorial utilizando o comando `System.out.println()`.
+
+Exercício 7 (Nível Médio - Variáveis):
+Crie um programa em Java que leia dois números e exiba o maior deles.
+
+Dica: Leia dois números utilizando a classe Scanner ou a função `JOptionPane.showInputDialog()` e compare-os utilizando uma estrutura condicional `if`. Utilize o operador de comparação `>` para verificar qual número é maior. Utilize o comando `System.out.println()` para exibir o resultado.
+
+Exercício 8 (Nível Médio - Loops):
+Escreva um programa em Java que exiba a sequência de Fibonacci até o décimo termo.
+
+Dica: Utilize um loop `for` ou `while` para calcular a sequência de Fibonacci. Declare três variáveis: duas para armazenar os dois últimos termos da sequência e uma para armazenar o valor atual. Inicialize os dois primeiros termos com os valores 0 e 1, e, em cada iteração, atualize os valores dos termos e exiba o valor atual utilizando o comando `System.out.println()`.
+
+Exercício 9 (Nível Médio - Loops):
+Implemente um programa em Java que exiba a tabuada de multiplicação de um número digitado pelo usuário.
+
+Dica: Leia um número utilizando a classe Scanner ou a função `JOptionPane.showInputDialog()` e utilize um loop `for` para percorrer os multiplicadores de 1 a 10. Multiplique o número pelo multiplicador em cada iteração e exiba o resultado utilizando o comando `System.out.println()`.
+
+Exercício 10 (Nível Médio - Condicionais):
+Crie um programa em Java que verifique se um ano é bissexto ou não.
+
+Dica: Leia um ano utilizando a classe Scanner ou a função `JOptionPane.showInputDialog()`. Utilize uma estrutura condicional `if` para verificar se o ano é bissexto. Verifique se o ano é divisível por 4 e não é divisível por 100, ou se é divisível por 400. Utilize o comando `System.out.println()` para exibir o resultado.
+
+pegar a string do numero e transforma em flota (parsefloat) ou inteiro (parseint)
+
