9d2843f45c5247e0584f51ddd3793a75d446f846
Rodrigo Borges <119867556+kaosbor@users.noreply.github.com>
2023-07-21 11:39:44 -0300

    21/07/2023 - 11:39
    Parei aqui:
     if (numeromin > numeromax) {
                System.out.println("O maior número é: " + numeromin);
            } else if (numeromin > numeromax) {
                System.out.println("O maior número é: " + numeromax);
            } else {
                System.out.println("Os números são iguais.");
            }

---
+++ Daniel.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Daniel {
+
+    public static void main(String[] args) { // método - main é palavra reservada
+        JFrame janela = new JFrame();
+        JLabel ronaldo = new JLabel();
+        janela.setSize(1000, 1000);
+        janela.setVisible(true);
+        janela.setTitle("Ronaldo");
+        ronaldo.setText("Jhalin Habbei");
+        janela.add(ronaldo);
+        ronaldo.setVisible(true);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
+
--- Exercicio1.java
+++ Exercicio1.java
@@ -4,7 +4,7 @@ import java.util.Random; // Para gerar numerais aleatórios
 
 public class Exercicio1 {
 
-    public static int gerarNumeroAleatorio(int min, int max) { // Outro método - nome gerarNumeroAleatorio
+    public static int gerarNumeroAleatorio(int min, int max) { // Função - nome gerarNumeroAleatorio
         Random random = new Random();
         return random.nextInt((max - min) + 1) + min;
     }
@@ -14,15 +14,16 @@ public class Exercicio1 {
         out.println("\033[h\033[2J"); // LIMPA A TELA
         Scanner scanner = new Scanner(System.in);
 
+        // ****************************************************************************************************
         // Exercício 1 (Nível Fácil - Variáveis):
         out.println("Exercício 1 (Nível Fácil - Variáveis):");
-        out.println("Digite um nota número: ");
-
+        out.println("Digite um número: ");
         int numero0 = gerarNumeroAleatorio(1, 100);
-        out.println("O número declarado foi " + numero0 + ".");
+        out.println("O número digitado foi " + numero0 + ".");
 
-        // Exercício 2 (Nível Fácil - Variáveis):
+        // ****************************************************************************************************
         out.print("\n");
+        // Exercício 2 (Nível Fácil - Variáveis):
         out.println("Exercício 2 (Nível Fácil - Variáveis):");
 
         out.print("Digite o 1º número: ");
@@ -42,15 +43,19 @@ public class Exercicio1 {
 
         out.println("A soma dos números " + numero1 + " + " + numero2 + " é " + soma + ".");
 
-        // Exercício 3 (Nível Fácil - Loops):
+        // ****************************************************************************************************
         out.print("\n");
+        // Exercício 3 (Nível Fácil - Loops):
+        ///// int numero = gerarNumeroAleatorio(1, 100);
         out.println("Exercício 3 (Nível Fácil - Loops):");
 
         out.print("Digite um número para início: ");
-        int inicio = scanner.nextInt();
+        int inicio = gerarNumeroAleatorio(1, 100);
+        // int incio = scanner.nextInt();
 
         out.print("Digite um número para limite: ");
-        int limite = scanner.nextInt();
+        int limite = gerarNumeroAleatorio(1, 100);
+        // int limite = scanner.nextInt();
 
         for (int i = inicio; i <= limite; i++) {
             out.print(i + " ");
@@ -58,33 +63,38 @@ public class Exercicio1 {
         out.print("\n");
         out.println("O número iniciou em " + inicio + " e terminou em " + limite + ".");
 
-        // Exercício 4 (Nível Fácil - Loops - Soma):
+        // ****************************************************************************************************
         out.print("\n");
+        // Exercício 4 (Nível Fácil - Loops - Soma):
         out.println("Exercício 4 (Nível Fácil - Loops - Soma):");
 
         out.print("Digite um número para início: ");
-        int iniciar = scanner.nextInt();
+        int iniciar = gerarNumeroAleatorio(1, 1000);
+        // int iniciar = scanner.nextInt();
 
         out.print("Digite um número para limite: ");
-        int limitar = scanner.nextInt();
+        int limitar = gerarNumeroAleatorio(1, 1000);
+        // int limitar = scanner.nextInt();
 
         int soma1 = 0;
 
         for (int i = 1; i <= limitar; i++) {
-            out.println("A soma se refere aos seguintes números " + i);
+            out.println("A soma se refere aos seguintes números:");
+            out.print(i);
             soma1 = soma1 + i; // ou += soma foi convertida para soma + i
         }
 
         out.println("O número iniciou em " + iniciar + " e terminou em " + limitar + ".");
         out.println("A soma dos números do inicio em " + iniciar + " com término em " + limitar
-                + " foi convertida para " + soma1 + ".");
+                + " é " + soma1 + ".");
 
-        // Exercício 5 (Nível Fácil - Condicionais):
+        // ****************************************************************************************************
         out.print("\n");
+        // Exercício 5 (Nível Fácil - Condicionais):
         out.println("Exercício 5 (Nível Fácil - Condicionais):");
 
         out.print("Digite a nota do aluno: ");
-        double nota = scanner.nextDouble();
+        int nota = gerarNumeroAleatorio(1, 10);
 
         if (nota >= 9) {
             out.println("A nota do aluno " + nota + " foi convertida para 'A'.");
@@ -97,33 +107,86 @@ public class Exercicio1 {
         } else if (nota >= 5) {
             out.println("A nota do aluno " + nota + " foi convertida para 'E'");
         } else {
-            out.println("Não irá ganhar TORTUGUITA!");
+            out.println("O aluno não irá ganhar TORTUGUITA!");
         }
 
         out.print("\n");
 
+        // ****************************************************************************************************
+        // Exercício 6 (Nível Médio - Variáveis - Fatorial):
         out.println("Exercício 6 (Nível Fácil - Variáveis - Fatorial:");
-        out.print("Digite um número fatorial: ");
-        int Receber = scanner.nextInt();
+        out.println("Digite um número fatorial: ");
+        int Receber = gerarNumeroAleatorio(1, 10);
+        // int Receber = scanner.nextInt();
         CalcularFatorial(Receber);
 
-        scanner.close();
-    }
+        // ****************************************************************************************************
+        // Exercíco Médio 7 - Variáveis - Fatorial):
+        out.println("// Exercíco Médio 7 - Variáveis - MAIOR NUMERO):");
+        out.println("Digite um número fatorial: ");
 
-    public static void CalcularFatorial(int Numero) {
-        // Exercício 6 (Nível Médio - Variáveis - Fatorial):
+        out.print("Digite o 1º número: ");
+        int numeromin = scanner.nextInt();
+        out.println("O 1º número é " + numeromin + ".");
+
+        out.print("Digite o 2º número: ");
+        int numeromax = scanner.nextInt();
+        out.println("O 2º número é " + numeromax + ".");
+
+
+        if (numeromin > numeromax) {
+            System.out.println("O maior número é: " + numeromin);
+        } else if (numeromin > numeromax) {
+            System.out.println("O maior número é: " + numeromax);
+        } else {
+            System.out.println("Os números são iguais.");
+        }
+
+        scanner.close();
+    out.print("\n");
+    }
+        
+
+    // ******************************************** FUNÇÕES *********************************************
+    
+    // Exercício 6 (Nível Médio - Variáveis - Fatorial):
+    public static void CalcularFatorial(int Numeral) { // Exercício 6 (Nível Médio - Variáveis - Fatorial):
         PrintStream out = System.out;
         int fatorial = 1;
 
-        for (int i = 1; i <= Numero; i++) {
-            out.println(i);
+        for (int i = 1; i <= Numeral; i++) {
+            /* out.println(i); */
             fatorial = fatorial * i; // ou += soma foi convertida para soma + i
         }
 
         // printf = printformat chama a variável %d que já é da linguagem (inteiro)
         // variável (fatorial) e o parâmetro (Numero)
-        out.printf("O fatorial de %d é " + fatorial, Numero);
+        out.println("O número escolhido para ser calculado é " + Numeral + " e seu fatorial é " + fatorial + ".");
+        out.printf("O fatorial de %d é " + fatorial, Numeral);
         out.print("\n");
-        out.printf("O fatorial do numero " + Numero + " é " + fatorial);
+        out.printf("O fatorial do numero " + Numeral + " é " + fatorial);
+    }
+    
+
+
+    // ****************************************************************************************************
+
+    // Exercíco Médio 7 - Variáveis - MAIOR NUMERO):
+
+    public static void ExibirMaiorNumero(int MaiorNumero) { // Exercício 7 (Nível Médio - Variáveis - MAIOR NÚMERO):
+        PrintStream out = System.out;
+
+
+
+
+        
+        /*
+         * Crie um programa em Java que leia dois números e exiba o maior deles.
+         * Dica: Leia dois números utilizando a classe Scanner ou a função
+         * `JOptionPane.showInputDialog()` e compare-os utilizando uma estrutura
+         * condicional `if`. Utilize o operador de comparação `>` para verificar qual
+         * número é maior. Utilize o comando `System.out.println()` para exibir o
+         * resultado.
+         */
     }
 }
